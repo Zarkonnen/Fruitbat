9e3c91fb3421480229b332a78aef7e539e3268d0
David Stark <david.stark@metalbeetle.com>
2011-03-24 22:21:24 +0000

    Added test for multiplex resiliency / sync from random backup crash.

--- test/com/metalbeetle/fruitbat/filestorage/ReliabilityTest.java
+++ test/com/metalbeetle/fruitbat/filestorage/ReliabilityTest.java
@@ -62,7 +62,6 @@ public class ReliabilityTest {
 					new EnhancedStore(backupS)
 			), new DummyProgressMonitor());
 			Document d = ms.create();
-			int id = d.getID();
 			d.change(l(DataChange.put("jam", "jam")));
 			ms.close();
 
@@ -93,6 +92,56 @@ public class ReliabilityTest {
 			assertEquals(2, ms.enabledStores());
 			assertEquals(1, ms.getIndex().search(m(p("jam", "")), 1).docs.size());
 			assertEquals(1, ms.enabledStores());
+		} finally {
+			Util.deleteRecursively(loc1);
+			Util.deleteRecursively(loc2);
+		}
+	}
+
+	@Test
+	public void testSaveCrashResiliency() throws FatalStorageException {
+		File loc1 = Util.createTempFolder();
+		File loc2 = Util.createTempFolder();
+		try {
+			IntentionallyCrashingFileStreamFactory crashy = new IntentionallyCrashingFileStreamFactory();
+			FileStore backupS = new FileStore(loc1, new DummyProgressMonitor(), crashy) {
+				@Override
+				/** Pretend to be fast so we get to be the store that gets read from. */
+				public int getLag() { return 100; }
+			};
+			FileStore mainS = new FileStore(loc2, new DummyProgressMonitor());
+			MultiplexStore ms = new MultiplexStore(l(
+					new EnhancedStore(mainS),
+					new EnhancedStore(backupS)
+			), new DummyProgressMonitor());
+			Document d = ms.create();
+			int id = d.getID();
+			d.change(l(DataChange.put("jam", "jam")));
+			crashy.crashAtOutput((byte) ':');
+			d.change(l(DataChange.put("cat", "cat")));
+			assertEquals(1, ms.enabledStores());
+			assertEquals(1, ms.getIndex().search(m(p("cat", "")), 1).docs.size());
+			ms.close();
+
+			backupS = new FileStore(loc1, new DummyProgressMonitor()) {
+				@Override
+				/** Pretend to be fast so we get to be the store that gets read from. */
+				public int getLag() { return 100; }
+			};
+			mainS = new FileStore(loc2, new DummyProgressMonitor());
+			ms = new MultiplexStore(l(
+					new EnhancedStore(mainS),
+					new EnhancedStore(backupS)
+			), new DummyProgressMonitor());
+
+			assertEquals(2, ms.enabledStores());
+			assertEquals(1, ms.getIndex().search(m(p("jam", "")), 1).docs.size());
+			assertEquals(1, ms.getIndex().search(m(p("cat", "")), 1).docs.size());
+			assertEquals(2, ms.enabledStores());
+
+			d = ms.get(id);
+			assertTrue(d.has("jam"));
+			assertTrue(d.has("cat"));
 		} finally {
 			Util.deleteRecursively(loc1);
 			Util.deleteRecursively(loc2);
